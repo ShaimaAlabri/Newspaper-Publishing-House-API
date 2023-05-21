6bd9b6739bed512c88b97f2c55131ce47558af4c
unknown <shaima96alabri@hotmail.com>
2023-05-20 23:39:27 -0700

    linked with Database

---
+++ src/test/java/com/example/Newspaper/Publishing/House/API/NewspaperPublishingHouseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Newspaper.Publishing.House.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspaperPublishingHouseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
