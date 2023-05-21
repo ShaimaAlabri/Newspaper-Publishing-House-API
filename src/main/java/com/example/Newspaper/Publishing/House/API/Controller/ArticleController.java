b626b3fab864cb00d8006b06f9f908781214f85e
unknown <shaima96alabri@hotmail.com>
2023-05-21 00:34:04 -0700

    add ArticleService code

--- src/main/java/com/example/Newspaper/Publishing/House/API/Controller/ArticleController.java
+++ src/main/java/com/example/Newspaper/Publishing/House/API/Controller/ArticleController.java
@@ -1,5 +1,12 @@
 package com.example.Newspaper.Publishing.House.API.Controller;
 
+import com.example.Newspaper.Publishing.House.API.Service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ArticleController {
+    @Autowired
+    ArticleService articleService;
 
 }
--- src/main/java/com/example/Newspaper/Publishing/House/API/Service/ArticleService.java
+++ src/main/java/com/example/Newspaper/Publishing/House/API/Service/ArticleService.java
@@ -1,4 +1,40 @@
 package com.example.Newspaper.Publishing.House.API.Service;
 
+import com.example.Newspaper.Publishing.House.API.Models.Article;
+import com.example.Newspaper.Publishing.House.API.Repository.ArticleRepository;
+import com.example.Newspaper.Publishing.House.API.ResponseObjects.GetArticleResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ArticleService {
+    @Autowired
+    ArticleRepository articleRepository;
+
+    public void saveArticle(Article article) {
+        articleRepository.save(article);
+    }
+
+    public List<Article> getArticle() {
+        return articleRepository.findAll();
+    }
+
+    public GetArticleResponse getArticleById(Long article) {
+        Optional<Article> optionalEmployee =  articleRepository.findById(articleId);
+        if(!optionalEmployee.isEmpty())
+        {
+            Article article1 = optionalArticle.get();
+
+
+            GetArticleResponse getArticleResponse = new GetArticleResponse(article1.getTitle(),article1.getContent(),article1.getAuthor(),article1.getPublishDate());
+            return articleRepository;
+        }
+
+        return null;
+
+    }
+
 }
