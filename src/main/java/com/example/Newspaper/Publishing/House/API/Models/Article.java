655d1db1b364ddb495db92fa19002cbb1212f85d
unknown <shaima96alabri@hotmail.com>
2023-05-20 23:52:19 -0700

    creat Models package & creating Article class , User class

---
+++ src/main/java/com/example/Newspaper/Publishing/House/API/Models/Article.java
@@ -0,0 +1,26 @@
+package com.example.Newspaper.Publishing.House.API.Models;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+@Data
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Long id;
+    String title;
+    String content;
+    String author;
+    Data publishDate;
+
+
+}
---
+++ src/main/java/com/example/Newspaper/Publishing/House/API/Models/User.java
@@ -0,0 +1,25 @@
+package com.example.Newspaper.Publishing.House.API.Models;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+
+@Getter
+@Setter
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Long id;
+    String username;
+    String password;
+    List<String> roles;
+
+}
